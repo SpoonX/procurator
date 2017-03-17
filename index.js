ffb0ead9ca0bd1221c7dc8c4ba3f12072a947753
RWOverdijk <r.w.overdijk@gmail.com>
2017-03-17 10:16:39 +0100

    chore(project): initial commit

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const replace = require('stream-replace');
 const fs      = require('fs');
 
 module.exports = parameters => replace(/\{\{\s?([\w-.]+?)(?:\s*\:\s*(?:'|")?([\w-. ]+?)(?:'|")?)?\s?}}/g, (match, parameter, defaultValue) => {
-  console.log(defaultValue);
   if (typeof parameters[parameter] !== 'undefined') {
     return parameters[parameter];
   }
